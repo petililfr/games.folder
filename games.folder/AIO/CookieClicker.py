3e5889a85382369b6b90daca3dd274191b7a9b2f
petililfr <148146282+petililfr@users.noreply.github.com>
2023-10-18 14:43:00 +0100

    Update CookieClicker.py

--- games.folder/AIO/CookieClicker.py
+++ games.folder/AIO/CookieClicker.py
@@ -1,1 +1,2 @@
-
+print("Please exit this application. This is not supposed to be here.")
+print("To get to cookie clicker, go to https://github.com/petililfr/games.folder/individuals/CookieClicker.py
